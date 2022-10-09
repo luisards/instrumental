d0c7dd61dcba4885a190e7be561691697a42f29d
Luisa Ribeiro <56673771+luisards@users.noreply.github.com>
2022-10-09 14:58:45 +0200

    Add fill-in-the-blanks option

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ genius_access_token = os.environ['GENIUS_ACCESS_TOKEN']
 # genius object
 genius = lg.Genius(genius_access_token)
 
-#@st.cache(show_spinner=False, allow_output_mutation=True, suppress_st_warning=True)
+
+# @st.cache(show_spinner=False, allow_output_mutation=True, suppress_st_warning=True)
 
 def main():
     menu = ['Home', 'About']
@@ -21,9 +22,9 @@ def main():
     st.title("Instrumental""\nLearn with music!")
 
     if choice == "Home":
-        #Nav search form
+        # Nav search form
         with st.form(key='searchform'):
-            nav1, nav2 = st.columns([2,1])
+            nav1, nav2 = st.columns([2, 1])
 
             with nav1:
                 search_term = st.text_input("Select an artist")
@@ -31,49 +32,36 @@ def main():
                 submit_search = st.form_submit_button(label='Search')
 
         selected_language = st.sidebar.selectbox("Select a language", options=['pt'])
-        selected_structures = st.sidebar.multiselect(
-            "Select which grammar structures you want to practice",
-            options=['PRON', 'NOUN', 'VERB'],
-            default=['PRON', 'NOUN', 'VERB'])
+        selected_structures = st.sidebar.selectbox(
+            "Select a grammar structure",
+            options=['PRON', 'NOUN', 'VERB'])
         models = load_models()
         selected_model = models[selected_language]
-        st.success("Looking for {}'s songs with {}.".format(search_term,selected_structures))
+        st.success("Looking for {}'s songs with {}.".format(search_term, selected_structures))
         blank = st.checkbox("Blank?")
-        col1, col2 = st.columns([2,1])
+        col1, col2 = st.columns([2, 1])
         with col1:
             if submit_search:
-                artist = genius.search_artist(search_term, max_songs=1, sort="title")
+                artist = genius.search_artist(search_term, max_songs=3, sort="title")
                 songs = artist.songs
-                #st.write(songs)
                 for i in songs:
-                    st.write(i.title)
-                    st.write(i.lyrics)
+                    title = i.title
                     doc = selected_model(i.lyrics)
-                    #with st.button("Song")
-                    tokens = process_text(doc, blank=blank)
+                    tokens = process_text(doc, title, selected_structures, blank=blank)
                     annotated_text(*tokens)
+                    if len(tokens) > 0:
+                        break
 
-def process_text(doc, blank=False):
+
+def process_text(doc, title, selected_structures, blank=False):
     tokens = []
     for token in doc:
-        if token.pos_ == "NOUN":
-            tokens.append((token.text, "Noun", "#faa"))
-        elif token.pos_ == "VERB":
-            tokens.append((token.text, "Verb", "#fda"))
-        elif token.pos_ == "PRON":
-            tokens.append((token.text, "Pron", "#afa"))
+        if token.pos_ == selected_structures:
+            tokens.append((" " + "___" * len(token.text) + " "))
         else:
-            tokens.append(" " + token.text + " ")
-
-        if blank:
-            blank_tokens = []
-            for token in tokens:
-                if type(token) == tuple:
-                    blank_tokens.append(("_" * len(token[0]), token[1], token[2]))
-                else:
-                    blank_tokens.append(token)
-            return blank_tokens
+            tokens.append((" " + token.text + " "))
     return tokens
+
 
 def load_models():
     portuguese_model = spacy.load("./models/pt/")
@@ -82,4 +70,4 @@ def load_models():
 
 
 if __name__ == '__main__':
-	main()
+    main()
